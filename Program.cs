f153453f8a0c66599269cc860fa6e3269616c46d
chanielm <chanieljm@gmail.com>
2025-02-18 17:44:55 -0500

    PrintGroup(), PlanSchoolVisit(), and schools added

--- Program.cs
+++ Program.cs
@@ -7,10 +7,18 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-RandomizeAnimals();
-string[,] group = AssignGroup();
-Console.WriteLine("School A");
-// PrintGroup(group);
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
+
+void PlanSchoolVisit(string name, int groups = 6)
+{
+    RandomizeAnimals();
+    string[,] group = AssignGroup(groups);
+    Console.WriteLine(name);
+    PrintGroup(group);
+}
+
 
 void RandomizeAnimals()
 {
@@ -26,17 +34,30 @@ void RandomizeAnimals()
     }
 }
 
-string[,] AssignGroup(int groups = 6) 
+string[,] AssignGroup(int groups = 6)
 {
-    string[,] result = new string[groups, pettingZoo.Length/groups];
+    string[,] result = new string[groups, pettingZoo.Length / groups];
     int start = 0;
 
     for (int i = 0; i < groups; i++)
     {
         for (int j = 0; j < result.GetLength(1); j++)
         {
-            result[i,j] = pettingZoo[start++];
+            result[i, j] = pettingZoo[start++];
         }
     }
     return result;
+}
+
+void PrintGroup(string[,] group)
+{
+    for (int i = 0; i < group.GetLength(0); i++)
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < group.GetLength(1); j++)
+        {
+            Console.Write($"{group[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
 }
